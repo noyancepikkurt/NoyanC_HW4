aac2bd09a0659c8e437efcb937693b6b49767122
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2023-06-06 14:46:09 +0300

    first commit

---
+++ NoyanC_HW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoyanC_HW4
+//
+//  Created by Noyan Çepikkurt on 6.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
